553c76176fa6fd3276ec508d23c1e68139ce0754
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-07-08 15:01:30 -0400

    Final commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -15,6 +15,38 @@
     "use strict";
 
 
+    // User class
+    var User = (function () {
+        function User(userName, userPassword) {
+            this.userName = userName;
+            this.userPassword = userPassword;
+        }
+        return User;
+
+    } ());
+
+    // display User Infomration on Console
+    function printInfo(User) {
+        console.log("===============================")
+        console.log("User Name : " + User.userName);
+        console.log("Password : " + User.userPassword);
+        console.log("===============================")
+    }
+
+    // eventhandler for Login submit event
+    function formSubmit(event) {
+        event.preventDefault();
+
+        var userName = document.getElementById("userName");
+        var userPassword = document.getElementById("userPassword");
+
+        var user = new User(userName.value, userPassword.value);
+        printInfo(user);
+        loginForm.reset();
+    }
+    // add to eventhandler
+    var loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", formSubmit);
 
 
 })();
